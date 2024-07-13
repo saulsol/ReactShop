25cfd41906750d9abb9cce4b364123e0ef25b9ec
saul <dlathf3210@naver.com>
2024-07-13 15:18:50 +0900

    0713

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,9 +1,49 @@
-import React from 'react';
+import React, {useState} from 'react';
+
+interface ProductType{
+    id: number;
+    name: string;
+    explanation: string,
+    price: number;
+}
+const App = () => {
+    const products: ProductType[] = [
+        {
+            id: 0,
+            name: "제품",
+            explanation: '해상도 지원',
+            price: 1200
+        },
+    ];
 
 
-function App() {
+    const handleSubmit=(event : any, someValue: number)=>{
+        event.preventDefault();
+        console.log(event.type)
+        console.log(someValue);
+    }
+
+    const someValue = 10;
+
   return (
-    <h1>쇼핑몰 앱 만들어보기</h1>
+      <>
+          <form onSubmit={(e)=>handleSubmit(e,someValue)}>
+              <input type={"text"} placeholder={"상품이름"} />
+              <input type={"text"} placeholder={"상품설명"}/>
+              <input type={"number"} placeholder={"상품가격"}></input>
+              <input type={"submit"} placeholder={"상품만들기"}/>
+          </form>
+
+        {products.map((product)=>(
+                <div key={product.id}>
+                    <div>{product.id}</div>
+                    <div>{product.name}</div>
+                    <div>{product.price}</div>
+                    <div>{product.explanation}</div>
+                </div>
+            )
+        )}
+      </>
   );
 }
 
